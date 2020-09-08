90f8a3b1b53af31402e1c758d435c4d47b0f703d
bharat_patil <patilbharat09@gmail.com>
2020-09-08 07:30:29 +0530

    Added attack function in game

--- app.js
+++ app.js
@@ -3,13 +3,45 @@ new Vue({
   data: {
     playerHealth: 100,
     monsterHealth: 100,
-    gameIsRunning: false
+    gameIsRunning: false,
   },
   methods: {
-    startGame: function() {
+    startGame: function () {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
-    }
-  }
+    },
+    attack: function () {
+      this.monsterHealth -= this.calculateDamge(3, 10);
+      if (this.checkWin()) {
+        return;
+      }
+      this.playerHealth -= this.calculateDamge(5, 12);
+      this.checkWin();
+    },
+    specialAttack: function () {},
+    heal: function () {},
+    giveUp: function () {},
+    calculateDamge: function (min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    checkWin: function () {
+      if (this.monsterHealth <= 0) {
+        if (confirm("You won! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      } else if (this.playerHealth <= 0) {
+        if (confirm("You lost! New Game?")) {
+          this.startGame();
+        } else {
+          this.gameIsRunning = false;
+        }
+        return true;
+      }
+      return false;
+    },
+  },
 });
